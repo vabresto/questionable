3f23670cf9c2e7a38820ed74cfe5d1c7ddb695cb
vabresto <77133146+vabresto@users.noreply.github.com>
2023-05-22 20:46:34 -0400

    Support `result` and `results` before falling back to stew

--- questionable/resultsbase.nim
+++ questionable/resultsbase.nim
@@ -2,6 +2,8 @@ template tryImport(module) = import module
 
 when compiles tryImport pkg/result:
   import pkg/result/../results
+elif compiles tryImport pkg/results:
+  import pkg/results/../results
 else:
   import pkg/stew/results
 
